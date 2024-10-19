494bc890bbfca8a8478b5610b44f22c482cfa248
Pancho1395 <A23310350@live.ceti.mx>
2024-10-19 01:05:41 +0000

    clase 2

--- include/TazoDorado.hpp
+++ include/TazoDorado.hpp
@@ -11,3 +11,4 @@ public:
         this ->flotando = true;
     }
 };
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,10 @@
+#include<iostream>
 #include<TazoDorado.hpp>
+
+
+int main(){
+    std::cout<<"Imprimir desde c++"<<std::endl;
+
+}
+
 
